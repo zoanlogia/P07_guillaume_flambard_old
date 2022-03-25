259cf374bcab4feee5eca6260a226374eba9541d
zoanlogia <g.flambard@gmail.com>
2022-03-25 17:23:27 +0000

    add some components, but PLS pour array in array

--- scripts/tools/ui.js
+++ scripts/tools/ui.js
@@ -1,4 +1,5 @@
 export const displayRecipes = (recipes) => {
+    
     const container = document.querySelector('.recipes__container')
     container.innerHTML = ''
     recipes.forEach(recipe => {
@@ -10,6 +11,64 @@ export const displayRecipes = (recipes) => {
 
 const card = (recipe) => {
     const div = document.createElement('div')
-    div.innerHTML = recipe.name
+    div.classList.add('card')
+    div.append(headingCard(recipe))
+    div.append(bodyCard(recipe));
+    div.append(row(recipe));
     return div
 }
+
+const headingCard = (recipe) => {
+    const div = document.createElement('div')
+    div.classList.add("img");
+    return div
+}
+
+const bodyCard = (recipe) => {
+    const div = document.createElement('div')
+    div.classList.add("row");
+    div.append(colCardName(recipe));
+    div.append(colCardTime(recipe));
+    return div
+}
+
+const colCardName = (recipe) => {
+    const div = document.createElement("div");
+    div.classList.add("col");
+    div.innerHTML = recipe.name;
+    return div;
+} 
+
+const colCardTime = (recipe) => {
+    const div = document.createElement("div");
+    div.classList.add("col");
+    div.classList.add("icon");
+    div.innerHTML =
+    `<img class='clock' src='../../assets/img/clock.svg' alt='clock' />` +
+    `<p class='time'>${recipe.time}</p>`;
+    return div;
+} 
+
+const row = (recipe) => {
+    const div = document.createElement('div')
+    div.classList.add('row')
+    div.append(colIngredients(recipe))
+    div.append(colDesc(recipe))
+    
+    return div
+}
+
+
+const colDesc = (recipe) => {
+  const div = document.createElement("div");
+  div.classList.add("desc");
+  div.innerHTML = recipe.description;
+  return div;
+}; 
+
+const colIngredients = (recipe) => {
+  const div = document.createElement("div");
+  div.classList.add("ingredients");
+    div.innerHTML = recipe.ingredients
+  return div;
+}; 
