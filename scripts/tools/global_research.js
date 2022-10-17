bc8b1c4a50bbb0d8792fcbd69c51b54b325f1cea
Guillaume Flambard <g.flambard@gmail.com>
2022-10-17 12:21:25 +0700

    seach global with boucle for OK on branch main

--- scripts/tools/global_research.js
+++ scripts/tools/global_research.js
@@ -3,10 +3,10 @@ import { displayError } from "../components/errorMessage.js";
 import { getRecipesStocked, setRecipesStocked } from "./storage.js";
 import { normalizeAccents } from "./regex.js";
 import { updateDropdowns } from "./updateDropdowns.js ";
+import { displayRecipes } from "./ui.js";
 
 export const globalSearch = () => {
   const searchbar = document.getElementById("searchbar");
-  const recipes = getRecipesStocked();
   const container = document.querySelector(".recipes__container");
 
   /**
@@ -14,46 +14,79 @@ export const globalSearch = () => {
    */
   searchbar.addEventListener("input", (e) => {
     if (e.target.value.length >= 3) {
+      const recipes = getRecipesStocked();
       container.innerHTML = "";
       for (let i = 0; i < recipes.length; i++) {
-        if (
-          recipes[i].name
-            .toLowerCase()
-            .includes(normalizeAccents(e.target.value.toLowerCase()))
-        ) {
-          container.append(card(recipes[i]));
-        }
-
-        else if (
-          recipes[i].ingredients.some((ingredient) => {
-            return ingredient.ingredient
+        if (recipes[i].display) {
+          if (
+            recipes[i].name
               .toLowerCase()
-              .includes(normalizeAccents(e.target.value.toLowerCase()));
-          })
-        ) {
-          container.append(card(recipes[i]));
-        } else if (
-          normalizeAccents(recipes[i].description)
-            .toLowerCase()
-            .includes(normalizeAccents(e.target.value.toLowerCase()))
-        ) {
-          container.append(card(recipes[i]));
-        }
-      }
-      if (container.innerHTML == "") {
-        container.append(displayError());
-      }
-    } else {
-      if (document.querySelectorAll(".tags").length > 0) {
-        container.innerHTML = "";
-        for (let i = 0; i < recipes.length; i++) {
-          if (recipes[i].display) {
+              .includes(normalizeAccents(e.target.value.toLowerCase()))
+          ) {
+            container.append(card(recipes[i]));
+          }
+          else if (
+            recipes[i].ingredients.some((ingredient) => {
+              return ingredient.ingredient
+                .toLowerCase()
+                .includes(normalizeAccents(e.target.value.toLowerCase()));
+            })
+          ) {
+            container.append(card(recipes[i]));
+          } else if (
+            normalizeAccents(recipes[i].description)
+              .toLowerCase()
+              .includes(normalizeAccents(e.target.value.toLowerCase()))
+          ) {
             container.append(card(recipes[i]));
           }
         }
       }
+      if (container.innerHTML == "") {
+        container.append(displayError());
+      } 
+    } else {
+
+      const ingsTags = Array.from(
+        document.querySelectorAll(".tag_ingredients > span")
+      ).map((ing) => ing.innerText.toLowerCase());
+      const ustsTags = Array.from(
+        document.querySelectorAll(".tag_ustensils > span")
+      ).map((ust) => ust.innerText.toLowerCase());
+      const appsTags = Array.from(
+        document.querySelectorAll(".tag_appliances > span")
+      ).map((app) => app.innerText.toLowerCase());
+
+      const DATA = getRecipesStocked();
+
+      for (let i = 0; i < DATA.length; i++) {
+        const recipeIngredients = DATA[i].ingredients.map((ing) =>
+          ing.ingredient.toLowerCase()
+        );
+        const recipeUstensils = DATA[i].ustensils.map((ustensil) =>
+          ustensil.toLowerCase()
+        );
+        const recipeAppliance = DATA[i].appliance.toLowerCase();
+
+        const recipeData = [
+          ...recipeIngredients,
+          recipeAppliance,
+          ...recipeUstensils,
+        ];
+        const tagsData = [...ingsTags, ...appsTags, ...ustsTags];
+
+        const allFounded = tagsData.every((el) => recipeData.includes(el));
+
+        if (allFounded) {
+          DATA[i].display = true;
+        } else {
+          DATA[i].display = false;
+        }
+        console.log(DATA);
+      }
+      setRecipesStocked(DATA);
+      displayRecipes();
     }
-    setRecipesStocked(recipes);
-    updateDropdowns()
-  });
+  })
+  updateDropdowns();
 }
