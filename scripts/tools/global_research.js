f77f38b4967814db2861413a54b33e7f24132f29
Guillaume Flambard <g.flambard@gmail.com>
2022-10-31 16:28:00 +0700

    loop for

--- scripts/tools/global_research.js
+++ scripts/tools/global_research.js
@@ -7,6 +7,95 @@ import { displayRecipes, removeAllselectedDropdowns } from "./ui.js";
 
 // Algo with a forEach
 
+// export const globalSearch = () => {
+//   const searchbar = document.getElementById("searchbar");
+//   const container = document.querySelector(".recipes__container");
+
+//   /**
+//    * @param {string} value - Valeur entrée dans le champ de recherche
+//    */
+//   searchbar.addEventListener("input", (e) => {
+//     if (e.target.value.length >= 3) {
+//       const recipes = getRecipesStocked();
+//       container.innerHTML = "";
+//       recipes.forEach((recipe) => {
+//         if (recipe.display) {
+//           if (
+//             recipe.name
+//               .toLowerCase()
+//               .includes(normalizeAccents(e.target.value.toLowerCase()))
+//           ) {
+//             container.append(card(recipe));
+//           }
+//           else if (
+//             recipe.ingredients.some((ingredient) => {
+//               return ingredient.ingredient
+//                 .toLowerCase()
+//                 .includes(normalizeAccents(e.target.value.toLowerCase()));
+//             })
+//           ) {
+//             container.append(card(recipe));
+//           } else if (
+//             normalizeAccents(recipe.description)
+//               .toLowerCase()
+//               .includes(normalizeAccents(e.target.value.toLowerCase()))
+//           ) {
+//             container.append(card(recipe));
+//           }
+//         }
+//       });
+//       if (container.innerHTML == "") {
+//         container.append(displayError());
+//       }
+//     } else {
+//       // updateDropdowns();
+
+//       const ingsTags = Array.from(
+//         document.querySelectorAll(".tag_ingredients > span")
+//       ).map((ing) => ing.innerText.toLowerCase());
+//       const ustsTags = Array.from(
+//         document.querySelectorAll(".tag_ustensils > span")
+//       ).map((ust) => ust.innerText.toLowerCase());
+//       const appsTags = Array.from(
+//         document.querySelectorAll(".tag_appliances > span")
+//       ).map((app) => app.innerText.toLowerCase());
+
+//       const DATA = getRecipesStocked();
+
+//       DATA.forEach((recipe) => {
+//         const recipeIngredients = recipe.ingredients.map((ing) =>
+//           ing.ingredient.toLowerCase()
+//         );
+//         const recipeUstensils = recipe.ustensils.map((ustensil) =>
+//           ustensil.toLowerCase()
+//         );
+//         const recipeAppliance = recipe.appliance.toLowerCase();
+
+//         const recipeData = [
+//           ...recipeIngredients,
+//           recipeAppliance,
+//           ...recipeUstensils,
+//         ];
+//         const tagsData = [...ingsTags, ...appsTags, ...ustsTags];
+
+//         const allFounded = tagsData.every((el) => recipeData.includes(el.toLowerCase()));
+
+//         if (allFounded) {
+//           recipe.display = true;
+//         } else {
+//           recipe.display = false;
+//         }
+//       });
+//       setRecipesStocked(DATA);
+//       displayRecipes();
+//       removeAllselectedDropdowns();
+//       updateDropdowns();
+//     }
+//   })
+// }
+
+// same function width loop FOR 
+
 export const globalSearch = () => {
   const searchbar = document.getElementById("searchbar");
   const container = document.querySelector(".recipes__container");
@@ -18,7 +107,8 @@ export const globalSearch = () => {
     if (e.target.value.length >= 3) {
       const recipes = getRecipesStocked();
       container.innerHTML = "";
-      recipes.forEach((recipe) => {
+      for (let i = 0; i < recipes.length; i++) {
+        const recipe = recipes[i];
         if (recipe.display) {
           if (
             recipe.name
@@ -26,8 +116,7 @@ export const globalSearch = () => {
               .includes(normalizeAccents(e.target.value.toLowerCase()))
           ) {
             container.append(card(recipe));
-          }
-          else if (
+          } else if (
             recipe.ingredients.some((ingredient) => {
               return ingredient.ingredient
                 .toLowerCase()
@@ -43,7 +132,7 @@ export const globalSearch = () => {
             container.append(card(recipe));
           }
         }
-      });
+      }
       if (container.innerHTML == "") {
         container.append(displayError());
       }
@@ -78,12 +167,12 @@ export const globalSearch = () => {
         ];
         const tagsData = [...ingsTags, ...appsTags, ...ustsTags];
 
-        const allFounded = tagsData.every((el) => recipeData.includes(el.toLowerCase()));
+        const allFounded = tagsData.every((el) =>
+          recipeData.includes(el ? el.toLowerCase() : "")
+        );
 
         if (allFounded) {
           recipe.display = true;
-        } else {
-          recipe.display = false;
         }
       });
       setRecipesStocked(DATA);
@@ -91,8 +180,10 @@ export const globalSearch = () => {
       removeAllselectedDropdowns();
       updateDropdowns();
     }
-  })
-}
+  });
+};
+
+
 
 
 
